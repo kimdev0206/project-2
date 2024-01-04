f223d9e0a410136ba8c778e95ea6a236569c4e10
Yongki Kim <ygflove@likelion.org>
2024-01-05 02:25:17 +0900

    Revert "add: 회원 API"

--- src/repositories/index.js
+++
@@ -1,8 +0,0 @@
-const UsersRepository = require("./users.repository");
-const database = require("../database");
-
-const usersRepository = new UsersRepository(database);
-
-module.exports = {
-  usersRepository,
-};
