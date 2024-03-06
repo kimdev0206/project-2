ed3f1ac65e936c30bfd35b97512249d36c6d2244
Yongki Kim <kimdev0206@gmail.com>
2024-03-07 05:13:24 +0900

    style: 접근 토큰 유효성 검사 미들웨어 적용

--- src/routes/orders.route.js
+++ src/routes/orders.route.js
@@ -2,7 +2,7 @@ module.exports = function ({ express, controller, middlewares }) {
   const router = express.Router();
   const { authMiddleware, validMiddleware } = middlewares;
 
-  router.use(authMiddleware.verifyToken);
+  router.use(authMiddleware.verifyAccessToken);
   router.post(
     "/",
     validMiddleware.validatePostOrder,
