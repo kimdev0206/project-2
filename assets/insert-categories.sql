4283850d031ffa98ed352455e716305abbd86a8e
Yongki Kim <kimdev0206@gmail.com>
2024-07-25 15:28:36 +0900

    remove: 카테고리 테이블

--- assets/insert-categories.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO 
-  `project-2`.categories (
-    category
-  ) 
-VALUES
-  ('IT'),
-  ('건강'),
-  ('교육');
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,5 @@
 const booksController = require("./books.controller");
 const cartBooksController = require("./cart-books.controller");
-const categoriesController = require("./categories.controller");
 const likesController = require("./likes.controller");
 const ordersController = require("./orders.controller");
 const promotionsController = require("./promotions.controller");
@@ -9,7 +8,6 @@ const usersController = require("./users.controller");
 module.exports = {
   booksController,
   cartBooksController,
-  categoriesController,
   likesController,
   ordersController,
   promotionsController,
